41fb3073495ba01206670c037fdf31c497378b34
masckmaster2007 <63162857+masckmaster2007@users.noreply.github.com>
2023-06-09 14:57:40 +0200

    Bug Origin

---
+++ bug.php
@@ -0,0 +1,7 @@
+<?php 
+$level = "Li7wak"; // any name
+$desc = "Li7wak"; // any desc
+$enc = base64_encode($desc); // base64 the desc
+$va = $_POST["levelID"]; // getting said level id
+echo("1:$va:2:$level $va:3:$enc:4:H4sIAAAAAAAAC6WQwQ3DIAxFF3IlfxsIUU6ZIQP8AbJChy_GPSZqpF7-A4yfDOfhXcCiNMIqnVYrgYQl8rDwBTZCVbkQRI3oVHbiDU6F2jMF_lesl4q4kw2PJMbovxLBQxTpM3-I6q0oHmXjzx7N0240cu5w0UBNtESRkble8uSLHjh8nTubmYJZ2MvMrEITEN0gEJMxlLiMZ28frmj:5:1:6:3935672:8:0:9:0:10:1:12:0:13:21:14:0:17::43:0:25::18:0:19:0:42:0:45:1:15:0:30:55610687:31:0:28:1 hour:29:1 hour:35:546561:36::37:0:38:0:39:50:46::47::40::27:AQcHBwEL#1bae6491cc87c72326abcbc0a7afaee139aa7088#f17c5a61f4ba1c7512081132459ddfaaa7c6f716");
+?>
